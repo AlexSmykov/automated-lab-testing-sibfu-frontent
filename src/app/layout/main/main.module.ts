39ce0deb36f17d389c730aac60cf75141bc34dc8
Alexey <smykovad@cifra-k.ru>
2023-12-19 05:42:12 +0700

    refactor: перенесен лайаут бара и контента в main-component

--- src/app/layout/main/main.module.ts
+++ src/app/layout/main/main.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
 
 import { MainComponent } from './main.component'
 import { MainRoutingModule } from './main-routing.module'
 import { FooterModule } from '../footer/footer.module'
 import { HeaderModule } from '../header/header.module'
+import { SideBarModule } from '../../components/side-bar/side-bar.module'
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [MainRoutingModule, FooterModule, HeaderModule],
+  imports: [
+    CommonModule,
+    MainRoutingModule,
+    FooterModule,
+    HeaderModule,
+    SideBarModule,
+  ],
   exports: [MainComponent],
 })
 export class MainModule {}
