6618f2738e26a35839e112cf02f73bf92cb86935
Alexey <smykovad@cifra-k.ru>
2024-05-06 01:50:41 +0700

    feat: пути для картинок

--- src/app/core/api/api-urls.ts
+++ src/app/core/api/api-urls.ts
@@ -6,6 +6,11 @@ const V1 = '/v1'
 
 const API_V1 = API + V1
 
+const IMAGE = '/image'
 const REGISTRATION = '/registration'
 
+// Регистрация / логин
 export const API_REGISTRATIONS = API_V1 + REGISTRATION
+
+// Utils
+export const API_IMAGE = API_V1 + IMAGE
