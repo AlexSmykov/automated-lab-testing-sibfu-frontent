aa39e46acf74ab8062c2c4f6f29c3eed37ed4898
Alexey <smykovad@cifra-k.ru>
2024-05-06 01:49:21 +0700

    feat: контрол для загрузки картинки (одной)

---
+++ src/app/shared/components/forms/image-input/image-input.component.ts
@@ -0,0 +1,51 @@
+import { Component, Input } from '@angular/core'
+import { FormControl, Validators } from '@angular/forms'
+import { NzUploadFile } from 'ng-zorro-antd/upload'
+
+import { ErrorService } from 'src/app/shared/services/error.service'
+import { ImageApiService } from 'src/app/core/api/image/image-api.service'
+import { TImage } from 'src/app/core/api/image/image-api.interface'
+
+import { map, Observable, tap } from 'rxjs'
+
+@Component({
+  selector: 'app-image-input',
+  templateUrl: './image-input.component.html',
+  styleUrls: ['./image-input.component.scss'],
+  providers: [ImageApiService],
+})
+export class ImageInputComponent {
+  @Input({ required: true }) control!: FormControl<number | null>
+  @Input() label?: string
+
+  loadedImage: NzUploadFile | null = null
+
+  get errorText(): string {
+    return this.errorService.checkError(this.control)
+  }
+
+  get isRequired(): boolean {
+    return this.control.hasValidator(Validators.required)
+  }
+
+  constructor(
+    private errorService: ErrorService,
+    private ImageApiService: ImageApiService
+  ) {}
+
+  beforeImageUpload = (file: NzUploadFile): Observable<boolean> => {
+    return this.ImageApiService.saveImage(file).pipe(
+      tap((image: TImage) => {
+        file.url = image.file
+        file.uid = image.id + ''
+        file.status = 'done'
+        this.loadedImage = file
+
+        this.control.patchValue(image.id)
+      }),
+      map(() => false)
+    )
+  }
+
+  removeImage(): void {}
+}
---
+++ src/app/shared/components/forms/image-input/image-input.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core'
+import { NzInputModule } from 'ng-zorro-antd/input'
+import { ReactiveFormsModule } from '@angular/forms'
+import { NzIconModule } from 'ng-zorro-antd/icon'
+import { NzFormModule } from 'ng-zorro-antd/form'
+import { NzGridModule } from 'ng-zorro-antd/grid'
+import { CommonModule } from '@angular/common'
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox'
+import { NzUploadModule } from 'ng-zorro-antd/upload'
+import { SvgIconComponent } from 'angular-svg-icon'
+
+import { ImageInputComponent } from 'src/app/shared/components/forms/image-input/image-input.component'
+import { IslandModule } from 'src/app/shared/components/island/island.module'
+
+@NgModule({
+  declarations: [ImageInputComponent],
+  imports: [
+    NzInputModule,
+    ReactiveFormsModule,
+    NzIconModule,
+    NzFormModule,
+    NzGridModule,
+    CommonModule,
+    NzCheckboxModule,
+    IslandModule,
+    NzUploadModule,
+    SvgIconComponent,
+  ],
+  exports: [ImageInputComponent],
+})
+export class ImageInputModule {}
