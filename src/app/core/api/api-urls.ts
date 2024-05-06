102ee3e12bc00d6106c3fe8b6eeb4b4478806082
Alexey <smykovad@cifra-k.ru>
2024-05-06 21:58:19 +0700

    feat: добавлен апи сервис для сущности курса

--- src/app/core/api/api-urls.ts
+++ src/app/core/api/api-urls.ts
@@ -7,10 +7,16 @@ const V1 = '/v1'
 const API_V1 = API + V1
 
 const IMAGE = '/image'
+const COURSE = '/course'
 const REGISTRATION = '/registration'
 
 // Регистрация / логин
 export const API_REGISTRATIONS = API_V1 + REGISTRATION
+
+// Курс
+
+export const API_COURSE = API_V1 + COURSE
+export const API_COURSE_ID = (id: number) => API_COURSE + '/' + id
 
 // Utils
 export const API_IMAGE = API_V1 + IMAGE
---
+++ src/app/core/api/course/course-api.interface.ts
@@ -0,0 +1,10 @@
+import {
+  TCourseCreateDto,
+  TCourseDto,
+} from 'src/app/core/api/course/course-api.dto'
+
+export type TCoursePost = TCourseCreateDto
+
+export type TCourseUpdate = TCourseCreateDto
+
+export type TCourse = TCourseDto
