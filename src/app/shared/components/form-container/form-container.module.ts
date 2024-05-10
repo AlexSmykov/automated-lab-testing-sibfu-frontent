eb5ec1eea47aee3738b195acd5ad6a3ac6e44780
Alexey <smykovad@cifra-k.ru>
2024-05-11 02:07:23 +0700

    feat: тайтл теперь не обязателен для форм контейнера

--- src/app/shared/components/form-container/form-container.module.ts
+++ src/app/shared/components/form-container/form-container.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzFormModule } from 'ng-zorro-antd/form';
+import { NgIf } from '@angular/common';
 
 import { FormContainerComponent } from 'src/app/shared/components/form-container/form-container.component';
 
 @NgModule({
   declarations: [FormContainerComponent],
-  imports: [NzInputModule, NzFormModule],
+  imports: [NzInputModule, NzFormModule, NgIf],
   exports: [FormContainerComponent],
 })
 export class FormContainerModule {}
