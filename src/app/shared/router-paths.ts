8ea5bd6d71f9f69a80859f787a5c5641dd9f7957
Alexey <smykovad@cifra-k.ru>
2024-05-11 02:14:10 +0700

    feat: добавлены пути для роутера для практики

--- src/app/shared/router-paths.ts
+++ src/app/shared/router-paths.ts
@@ -6,17 +6,23 @@ enum ERoutes {
   REGISTRATION = 'REGISTRATION',
   COURSES = 'COURSES',
   COURSES_ID = 'COURSES_ID',
-  PRACTICE = 'PRACTICE',
+  PRACTICES = 'PRACTICES',
+  PRACTICES_ID = 'PRACTICES_ID',
   COURSE_CREATE = 'COURSE_CREATE',
+  PRACTICE_CREATE = 'PRACTICE_CREATE',
+}
+
+export enum ERoutesIds {
+  COURSE_ID = 'courseId',
+  PRACTICE_ID = 'practiceId',
 }
 
 enum ERoutesParts {
-  ID = ':id',
   MAIN = 'main',
   LOGIN = 'login',
   REGISTRATION = 'registration',
   COURSES = 'courses',
-  PRACTICE = 'practice',
+  PRACTICES = 'practices',
   CREATE = 'create',
 }
 
@@ -25,37 +31,51 @@ export const EPartialRoutes: TKeyInEnum<ERoutes, string> = {
   [ERoutes.LOGIN]: [ERoutesParts.LOGIN].join('/'),
   [ERoutes.REGISTRATION]: [ERoutesParts.REGISTRATION].join('/'),
   [ERoutes.COURSES]: [ERoutesParts.COURSES].join('/'),
-  [ERoutes.COURSES_ID]: [ERoutesParts.ID].join('/'),
+  [ERoutes.COURSES_ID]: [':' + ERoutesIds.COURSE_ID].join('/'),
   [ERoutes.COURSE_CREATE]: [ERoutesParts.CREATE].join('/'),
-  [ERoutes.PRACTICE]: [
-    ERoutesParts.COURSES,
-    ERoutesParts.PRACTICE,
-    ERoutesParts.ID,
-  ].join('/'),
+  [ERoutes.PRACTICES]: [ERoutesParts.PRACTICES].join('/'),
+  [ERoutes.PRACTICES_ID]: [':' + ERoutesIds.PRACTICE_ID].join('/'),
+  [ERoutes.PRACTICE_CREATE]: [ERoutesParts.CREATE].join('/'),
 };
 
-export const EFullRoutes: TKeyInEnum<ERoutes, any> = {
+export const EFullRoutes = {
   [ERoutes.MAIN]: ['/', ERoutesParts.MAIN],
   [ERoutes.LOGIN]: ['/', ERoutesParts.LOGIN],
   [ERoutes.REGISTRATION]: ['/', ERoutesParts.REGISTRATION],
   [ERoutes.COURSES]: ['/', ERoutesParts.MAIN, ERoutesParts.COURSES],
+  [ERoutes.PRACTICES]: (courseId: number) => [
+    '/',
+    ERoutesParts.MAIN,
+    ERoutesParts.COURSES,
+    courseId,
+    ERoutesParts.PRACTICES,
+  ],
   [ERoutes.COURSES_ID]: (id: number) => [
     '/',
     ERoutesParts.MAIN,
     ERoutesParts.COURSES,
     id,
   ],
-  [ERoutes.PRACTICE]: (id: number) => [
+  [ERoutes.PRACTICES_ID]: (courseId: number, id: number) => [
     '/',
     ERoutesParts.MAIN,
     ERoutesParts.COURSES,
-    ERoutesParts.PRACTICE,
+    courseId,
+    ERoutesParts.PRACTICES,
     id,
   ],
   [ERoutes.COURSE_CREATE]: [
     '/',
     ERoutesParts.MAIN,
     ERoutesParts.COURSES,
+    ERoutesParts.CREATE,
+  ],
+  [ERoutes.PRACTICE_CREATE]: (courseId: number) => [
+    '/',
+    ERoutesParts.MAIN,
+    ERoutesParts.COURSES,
+    courseId,
+    ERoutesParts.PRACTICES,
     ERoutesParts.CREATE,
   ],
 };
