e40ede27faae5d1055693c9209a945548d8d0276
Alexey <smykovad@cifra-k.ru>
2024-05-12 00:11:43 +0700

    feat: добавлен условие для редактирование формы курса

--- src/app/pages/course-form-page/course-form-page.component.ts
+++ src/app/pages/course-form-page/course-form-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { UntilDestroy } from '@ngneat/until-destroy';
+import { Component, OnInit } from '@angular/core';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { FormBuilder, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { TFormGroupValue } from 'src/app/shared/interfaces/mapped-types.interface';
 import { CourseApiService } from 'src/app/core/api/course/course-api.service';
-import { EFullRoutes } from 'src/app/shared/router-paths';
+import { EFullRoutes, ERoutesIds } from 'src/app/shared/router-paths';
 import { TCourseFormValue } from 'src/app/pages/course-form-page/course-form-page.interface';
 
 @UntilDestroy()
@@ -15,7 +15,7 @@ import { TCourseFormValue } from 'src/app/pages/course-form-page/course-form-pag
   styleUrls: ['./course-form-page.component.scss'],
   providers: [CourseApiService],
 })
-export class CourseFormPageComponent {
+export class CourseFormPageComponent implements OnInit {
   createCourseForm: TFormGroupValue<TCourseFormValue> = this.fb.group({
     name: this.fb.control('', {
       nonNullable: true,
@@ -36,11 +36,47 @@ export class CourseFormPageComponent {
     // image: this.fb.control<number | null>(null),
   });
 
+  isEdit: boolean = false;
+  existenceCourseId: string | null = null;
+
   constructor(
     private fb: FormBuilder,
     private router: Router,
+    private activatedRoute: ActivatedRoute,
     private courseApiService: CourseApiService
   ) {}
+
+  ngOnInit(): void {
+    this.subOnRouteParams();
+  }
+
+  subOnRouteParams(): void {
+    this.activatedRoute.params
+      .pipe(untilDestroyed(this))
+      .subscribe((params) => {
+        const courseId: string = params[ERoutesIds.COURSE_ID];
+        this.isEdit = true;
+
+        if (courseId) {
+          this.courseApiService.get(courseId).subscribe((data) => {
+            this.createCourseForm.patchValue(data);
+            this.existenceCourseId = data.id;
+          });
+        }
+      });
+  }
+
+  createOrEditCourse(): void {
+    this.isEdit ? this.editCourse() : this.createCourse();
+  }
+
+  editCourse(): void {
+    this.courseApiService
+      .update(this.createCourseForm.getRawValue(), this.existenceCourseId!)
+      .subscribe(() => {
+        this.router.navigate(EFullRoutes.COURSES);
+      });
+  }
 
   createCourse(): void {
     this.courseApiService
