322edcc4e8dbd75d16a6ab0c8ce000b7020174d9
Alexey <smykovad@cifra-k.ru>
2024-05-12 04:13:59 +0700

    feat: подключено получение моих курсов

--- src/app/pages/courses-page/courses-page.component.ts
+++ src/app/pages/courses-page/courses-page.component.ts
@@ -2,26 +2,33 @@ import { Component, OnInit } from '@angular/core';
 
 import { RoleService } from 'src/app/core/role/role.service';
 import { EFullRoutes } from 'src/app/shared/router-paths';
-import { CoursesPageService } from 'src/app/pages/courses-page/courses-page.service';
+import { CourseApiService } from 'src/app/core/api/course/course-api.service';
+import { TCourse } from 'src/app/core/api/course/course-api.interface';
+
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-courses-page',
   templateUrl: './courses-page.component.html',
   styleUrls: ['./courses-page.component.scss'],
-  providers: [CoursesPageService],
+  providers: [CourseApiService],
 })
 export class CoursesPageComponent implements OnInit {
-  courses$ = this.homePageService.courses$;
+  private _courses$ = new BehaviorSubject<TCourse[] | null>(null);
+
+  courses$ = this._courses$.asObservable();
 
   isTeacher = this.roleService.isTeacher;
 
   constructor(
-    private homePageService: CoursesPageService,
+    private courseApiService: CourseApiService,
     private roleService: RoleService
   ) {}
 
   ngOnInit(): void {
-    this.homePageService.getCoursesAction();
+    this.courseApiService.getAll().subscribe((courses) => {
+      this._courses$.next(courses);
+    });
   }
 
   protected readonly EFullRoutes = EFullRoutes;
