01bc38a6acb43896484af33d1d842448bbd55e1b
Alexey <smykovad@cifra-k.ru>
2024-05-18 23:37:27 +0700

    fix: исправлено редактирование картинки в курсе

--- src/app/core/api/course/course-api.transformer.ts
+++ src/app/core/api/course/course-api.transformer.ts
@@ -11,18 +11,28 @@ import {
 import { API_STATIC_IMAGES } from 'src/app/core/api/api-urls';
 
 export function serializeCoursePost(data: TCoursePost): TCourseCreateDto {
-  return data;
+  return {
+    name: data.name,
+    description: data.description,
+    image_id: data.imageId,
+  };
 }
 
 export function serializeCourseUpdate(data: TCourseUpdate): TCourseUpdateDto {
-  return data;
+  return {
+    name: data.name,
+    description: data.description,
+    image_id: data.imageId,
+  };
 }
 
 export function deserializeCourse(dto: TCourseDto): TCourse {
+  console.log(API_STATIC_IMAGES + dto.image_id);
   return {
     id: dto.id,
     name: dto.name,
     description: dto.description,
     image: dto.image_id ? API_STATIC_IMAGES + dto.image_id : '',
+    imageId: dto.image_id,
   };
 }
