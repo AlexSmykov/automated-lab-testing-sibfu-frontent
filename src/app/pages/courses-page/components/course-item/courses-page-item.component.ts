4e37d95e1e55388de306dce407f0c38b64c884aa
Alexey <smykovad@cifra-k.ru>
2024-05-18 23:34:13 +0700

    style: убрана кнопка удаления с карточки курса в главном меню

--- src/app/pages/courses-page/components/course-item/courses-page-item.component.ts
+++ src/app/pages/courses-page/components/course-item/courses-page-item.component.ts
@@ -12,13 +12,8 @@ import { TCourse } from 'src/app/core/api/course/course-api.interface';
 })
 export class CoursesPageItemComponent {
   @Input({ required: true }) course!: TCourse;
+
   @Output() courseClick = new EventEmitter<void>();
 
   EFullRoutes = EFullRoutes;
-
-  constructor(private courseItemService: CoursesPageItemService) {}
-
-  deleteCourse(): void {
-    this.courseItemService.deleteCourse(this.course.id).subscribe();
-  }
 }
