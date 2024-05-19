03b649a467dcf57b229e76ee2a258c923cdd22d2
Alexey <smykovad@cifra-k.ru>
2024-05-19 07:53:17 +0700

    feat: обновлен компонент острова

--- src/app/shared/components/island/island.module.ts
+++ src/app/shared/components/island/island.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { NzInputModule } from 'ng-zorro-antd/input';
+import { NgStyle } from '@angular/common';
 
 import { IslandComponent } from 'src/app/shared/components/island/island.component';
 
 @NgModule({
   declarations: [IslandComponent],
-  imports: [NzInputModule],
+  imports: [NzInputModule, NgStyle],
   exports: [IslandComponent],
 })
 export class IslandModule {}
